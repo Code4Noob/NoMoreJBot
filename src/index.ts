26be112192084e592d510054e49bb5991ea81886
thingasd <thingasd@gmail.com>
2022-12-06 10:50:46 +0800

    init bot

---
+++ src/index.ts
@@ -0,0 +1,40 @@
+import "dotenv/config";
+
+import { Context, Markup, Telegraf } from "telegraf";
+
+import { Update } from "typegram";
+import { message } from "telegraf/filters";
+
+const bot: Telegraf<Context<Update>> = new Telegraf(
+  process.env.BOT_TOKEN as string
+);
+
+bot.start((ctx) => {
+  ctx.reply("Hello " + ctx.from.first_name + "!");
+});
+bot.help((ctx) => {
+  ctx.reply("Send /start to receive a greeting");
+  ctx.reply("Send /keyboard to receive a message with a keyboard");
+  ctx.reply("Send /quit to stop the bot");
+});
+bot.command("quit", (ctx) => {
+  // Explicit usage
+  ctx.telegram.leaveChat(ctx.message.chat.id);
+  // Context shortcut
+  ctx.leaveChat();
+});
+bot.command("keyboard", (ctx) => {
+  ctx.reply(
+    "Keyboard",
+    Markup.inlineKeyboard([
+      Markup.button.callback("First option", "first"),
+      Markup.button.callback("Second option", "second"),
+    ])
+  );
+});
+bot.on(message("sticker"), (ctx) => ctx.reply("👍"));
+
+bot.launch();
+
+process.once("SIGINT", () => bot.stop("SIGINT"));
+process.once("SIGTERM", () => bot.stop("SIGTERM"));
