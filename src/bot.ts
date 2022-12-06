9bfc12a375a4be4e78d0ea8b773f55589b289cbe
thingasd <thingasd@gmail.com>
2022-12-06 17:14:29 +0800

    Mongo DB config

--- src/bot.ts
+++ src/bot.ts
@@ -1,14 +1,15 @@
-import "dotenv/config";
-
 import { Context, Input, Markup, Telegraf } from "telegraf";
 
 import { Update } from "typegram";
+import client from "./db";
 import { message } from "telegraf/filters";
 import { v4 as uuidv4 } from "uuid";
 
 const bot: Telegraf<Context<Update>> = new Telegraf(
   process.env.BOT_TOKEN as string
 );
+
+const db = client.db(process.env.DBNAME);
 
 bot.start((ctx) => {
   ctx.reply("Hello " + ctx.from.first_name + "!");
@@ -18,16 +19,19 @@ bot.help((ctx) => {
 });
 // Commands
 bot.command("quit", (ctx) => {
-  bot.telegram.getChatAdministrators(ctx.chat.id).then((data) => {
-    // TODO: admin middleware to allow admin remove bot
-    if (!data || !data.length) return;
-    if (data.some((admin) => admin.user.id === ctx.from.id)) {
-      ctx.telegram.leaveChat(ctx.message.chat.id);
-      ctx.leaveChat();
-    } else {
-      ctx.reply("踢你老母臭");
-    }
-  }).catch(console.log);
+  bot.telegram
+    .getChatAdministrators(ctx.chat.id)
+    .then((data) => {
+      // TODO: admin middleware to allow admin remove bot
+      if (!data || !data.length) return;
+      if (data.some((admin) => admin.user.id === ctx.from.id)) {
+        ctx.telegram.leaveChat(ctx.message.chat.id);
+        ctx.leaveChat();
+      } else {
+        ctx.reply("踢你老母臭");
+      }
+    })
+    .catch(console.log);
 });
 bot.command("keyboard", (ctx) => {
   ctx.reply(
@@ -44,17 +48,22 @@ bot.command("picture", async (ctx) => {
   );
 });
 // Mentions
-bot.mention(process.env.BOT_NAME as string, (ctx)=>{
-    ctx.reply('👍');
-})
+bot.mention(process.env.BOT_NAME as string, (ctx) => {
+  ctx.reply("👍");
+});
 // Actions
-bot.action('first', (ctx)=>{
-    ctx.reply("first");
-})
-bot.action('second', (ctx)=>{
-    ctx.reply("second");
-})
+bot.action("first", (ctx) => {
+  ctx.reply("first");
+});
+bot.action("second", (ctx) => {
+  ctx.reply("second");
+});
 // bot.on(message("sticker"), (ctx) => ctx.reply("👍"));
 bot.hears(/\b(hi)\b/i, (ctx) => ctx.reply("Hey there"));
-
+bot.command("db", async (ctx) => {
+    const collection = db.collection('documents');
+    const findResult = await collection.find({}).toArray();
+    console.log(findResult)
+    ctx.reply(findResult[0].hello)
+});
 export default bot;
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,5 @@
+import "dotenv/config";
+
 import bot from './bot';
 
 bot.launch()
