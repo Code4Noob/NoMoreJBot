3fc3e7908ff0e47b6f30655e1bd6a58afcd7e468
tomho <thingasd@gmail.com>
2024-11-27 00:48:14 +0800

    mongo

--- src/db.ts
+++ src/db.ts
@@ -2,7 +2,7 @@ import { connect, disconnect, set } from "mongoose";
 
 // Connection URL
 const url =
-  process.env.MONOGOURL ??
+  process.env.MONGOURL ??
   `mongodb://${process.env.MONGOUSER}:${process.env.MONGOPASSWORD}@${process.env.MONGOHOST}:${process.env.MONGOPORT}`;
 set("strictQuery", true);
 
